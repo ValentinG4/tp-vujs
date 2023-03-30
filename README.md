732faeb2a423851c895eb26a18cc9f2e2efb13ee
ValentinG4 <91123767+ValentinG4@users.noreply.github.com>
2023-03-30 09:17:44 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# tp-vujs
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-c1e2tm)
---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <h1>My app</h1>
+  <div calss="row">
+    <div class="column">
+      <span v-html="fullUsername"></span>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="column">
+      <form>
+        <label for="firstname">Prénom :</label>
+        <input
+          v-model.lazy="firstName"
+          type="text"
+          id="firstname"
+          name="firstname"
+          required
+          maxlength=""
+          size="10"
+        />
+        <label for="lastname">Nom de famille :</label>
+        <input
+          v-model.lazy="lastName"
+          type="text"
+          id="lastname"
+          name="lastname"
+          required
+          maxlength="15"
+          size="10"
+        />
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      firstName: null,
+      lastName: null,
+      edit: true
+    }
+  },
+  computed: {
+    fullUsername() {
+      console.log('computed');
+      if (this.firstName && this.lastName){
+        return <strong>Utilisateur connecté : </strong> ${this.firstName} ${this.lastName}
+      }
+      return '<strong>Utilisateur non défini</strong>'
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style></style>
